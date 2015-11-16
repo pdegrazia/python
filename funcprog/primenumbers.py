435541cb182bb69e762780fbf8a471761f3b8f91
pdegrazia <p.degrazia@gmail.com>
2015-11-16 23:19:33 +0000

    calculate if number is prime using functional programming

--- funcprog/primenumbers.py
+++ funcprog/primenumbers.py
@@ -5,3 +5,19 @@ finding the prime numbers smaller than a given number n
 """
 __author__ = 'paolo'
 
+
+def has_dividends(n):
+    dividers=filter(lambda x:n%x == 0,range(2,n))
+    return dividers
+
+def is_prime(n):
+    if has_dividends(n):
+        print 'number is not prime'
+    else:
+        print 'number is prime'
+
+def main():
+    is_prime(10)
+
+if __name__ == '__main__':
+    main()
